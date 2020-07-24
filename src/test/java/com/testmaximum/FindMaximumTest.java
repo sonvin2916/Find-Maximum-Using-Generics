c9eed221797fdcc688c9e118a26228cd1d0c9129
sonvin2916 <sonalchougule07@gmail.com>
2020-07-24 17:56:05 +0530

    [Sonal]Added : Find Maximum of 3 variables using generics

---
+++ src/test/java/com/testmaximum/FindMaximumTest.java
@@ -0,0 +1,26 @@
+package com.testmaximum;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class FindMaximumTest {
+    @Test
+    public void givenMaxNumber_AtFirstPosition_ShouldReturnSameNumber(){
+        FindMaximum findMaximum = new FindMaximum();
+        int result = findMaximum.testMax(80,20,50);
+        Assert.assertEquals(80,result);
+    }
+    @Test
+    public void givenMaxNumber_AtSecondPosition_ShouldReturnSameNumber(){
+        FindMaximum findMaximum = new FindMaximum();
+        int result = findMaximum.testMax(20,80,50);
+        Assert.assertEquals(80,result);
+    }
+    @Test
+    public void givenMaxNumber_AtThirdPosition_ShouldReturnSameNumber(){
+        FindMaximum findMaximum = new FindMaximum();
+        int result = findMaximum.testMax(50,20,80);
+        Assert.assertEquals(80,result);
+    }
+
+}
