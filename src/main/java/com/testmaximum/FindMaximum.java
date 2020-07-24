99e15f7aa64ce520dbef055fd28215fde1b78504
sonvin2916 <sonalchougule07@gmail.com>
2020-07-24 18:22:57 +0530

    [Sonal]Added : Find maximum of 3 float variables

--- src/main/java/com/testmaximum/FindMaximum.java
+++ src/main/java/com/testmaximum/FindMaximum.java
@@ -10,4 +10,13 @@ public class FindMaximum {
             max=c;
         return  max;
     }
+    public static Float testMax(Float a, Float b, Float c)
+    {
+        Float max=a;
+        if(b.compareTo(max)>0)
+            max=b;
+        if(c.compareTo(max)>0)
+            max=c;
+        return max;
+    }
 }
--- src/test/java/com/testmaximum/FindMaximumTest.java
+++ src/test/java/com/testmaximum/FindMaximumTest.java
@@ -5,21 +5,43 @@ import org.junit.Test;
 
 public class FindMaximumTest {
     @Test
-    public void givenMaxNumber_AtFirstPosition_ShouldReturnSameNumber(){
+    public void givenIntegerMaxNumber_AtFirstPosition_ShouldReturnSameNumber(){
         FindMaximum findMaximum = new FindMaximum();
         int result = findMaximum.testMax(80,20,50);
         Assert.assertEquals(80,result);
     }
     @Test
-    public void givenMaxNumber_AtSecondPosition_ShouldReturnSameNumber(){
+    public void givenIntegerMaxNumber_AtSecondPosition_ShouldReturnSameNumber(){
         FindMaximum findMaximum = new FindMaximum();
         int result = findMaximum.testMax(20,80,50);
         Assert.assertEquals(80,result);
     }
     @Test
-    public void givenMaxNumber_AtThirdPosition_ShouldReturnSameNumber(){
+    public void givenIntegerMaxNumber_AtThirdPosition_ShouldReturnSameNumber(){
         FindMaximum findMaximum = new FindMaximum();
         int result = findMaximum.testMax(50,20,80);
         Assert.assertEquals(80,result);
     }
+    @Test
+    public void givenFloatMaxNumber_AtFirstPosition_ShouldReturnSameNumber() {
+        FindMaximum findMaximum = new FindMaximum();
+        float result = findMaximum.testMax(56.12f,30.16f,28.14f);
+       Assert.assertEquals(56.12f, result, 1.0);
+
+    }
+    @Test
+    public void givenFloatMaxNumber_AtSecondPosition_ShouldReturnSameNumber() {
+        FindMaximum findMaximum = new FindMaximum();
+        float result = findMaximum.testMax(30.16f,56.12f,28.14f);
+        Assert.assertEquals(56.12f, result, 1.0);
+    }
+    @Test
+    public void givenFloatMaxNumber_AtThirdPosition_ShouldReturnSameNumber() {
+        FindMaximum findMaximum = new FindMaximum();
+        float result = findMaximum.testMax(30.16f,28.14f,56.12f);
+        Assert.assertEquals(56.12f, result, 1.0);
+    }
+
+
+
 }
