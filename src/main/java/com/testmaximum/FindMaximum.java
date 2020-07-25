24f7b01c64dd4edb6f04e0a0e74e7f9d03806c2a
sonvin2916 <sonalchougule07@gmail.com>
2020-07-25 20:22:16 +0530

    [Sonal]Added:Generic class to take 3 variables of generic type

--- src/main/java/com/testmaximum/FindMaximum.java
+++ src/main/java/com/testmaximum/FindMaximum.java
@@ -1,12 +1,47 @@
 package com.testmaximum;
 
-public class FindMaximum {
-    public static <E extends Comparable<E>> E testMax(E firstNumber, E secondNumber, E thirdNumber) {
+public class FindMaximum <E extends Comparable<E>>
+{
+    E myXArray, myYArray, myZArray;
+
+    public FindMaximum(E myXArray, E myYArray, E myZArray)
+    {
+        this.myXArray = myXArray;
+        this.myYArray = myYArray;
+        this.myZArray = myZArray;
+    }
+
+    public E testMax()
+    {
+        return testMax(myXArray, myYArray, myZArray);
+    }
+
+    public static <E extends Comparable<E>> E testMax(E firstNumber, E secondNumber, E thirdNumber)
+    {
         E max = firstNumber;
-        if (secondNumber.compareTo(max) > 0)
+        if (secondNumber.compareTo(max) > 0) {
             max = secondNumber;
-        if (thirdNumber.compareTo(max) > 0)
+        }
+        if (thirdNumber.compareTo(max) > 0) {
             max = thirdNumber;
+        }
         return max;
+
+    }
+
+    public static void main(String args[])
+    {
+
+        Integer firstInt = 50, secondInt = 80, thirdInt = 20;
+        Float firstFloat = 56.12f, secondFloat = 30.16f, thirdFloat = 28.14f;
+        String firstString = "Banana", secondString = "Orange", thirdString = "Mango";
+        new FindMaximum<Integer>(firstInt, secondInt, thirdInt);
+        new FindMaximum<Float>(firstFloat, secondFloat, thirdFloat);
+        new FindMaximum<String>(firstString, secondString, thirdString);
     }
 }
+
+
+
+
+
--- src/test/java/com/testmaximum/FindMaximumTest.java
+++ src/test/java/com/testmaximum/FindMaximumTest.java
@@ -6,58 +6,57 @@ import org.junit.Test;
 public class FindMaximumTest {
     @Test
     public void givenIntegerMaxNumber_AtFirstPosition_ShouldReturnSameNumber(){
-        FindMaximum findMaximum = new FindMaximum();
-
-        int result = findMaximum.testMax(80,20,50);
+        FindMaximum findMaximum = new FindMaximum(80,20,50);
+        Comparable result = findMaximum.testMax();
         Assert.assertEquals(80,result);
     }
     @Test
     public void givenIntegerMaxNumber_AtSecondPosition_ShouldReturnSameNumber(){
-        FindMaximum findMaximum = new FindMaximum();
-        int result = findMaximum.testMax(20,80,50);
+        FindMaximum findMaximum = new FindMaximum(50,80,20);
+        Comparable result = findMaximum.testMax();
         Assert.assertEquals(80,result);
     }
     @Test
     public void givenIntegerMaxNumber_AtThirdPosition_ShouldReturnSameNumber(){
-        FindMaximum findMaximum = new FindMaximum();
-        int result = findMaximum.testMax(50,20,80);
+        FindMaximum findMaximum = new FindMaximum(20,50,80);
+        Comparable result = findMaximum.testMax();
         Assert.assertEquals(80,result);
     }
     @Test
     public void givenFloatMaxNumber_AtFirstPosition_ShouldReturnSameNumber() {
-        FindMaximum findMaximum = new FindMaximum();
-        float result = findMaximum.testMax(56.12f,30.16f,28.14f);
-       Assert.assertEquals(56.12f, result, 1.0);
+        FindMaximum findMaximum = new FindMaximum(56.12f,30.16f,28.14f);
+        Comparable result = findMaximum.testMax();
+        Assert.assertEquals(56.12f,result);
 
     }
     @Test
     public void givenFloatMaxNumber_AtSecondPosition_ShouldReturnSameNumber() {
-        FindMaximum findMaximum = new FindMaximum();
-        float result = findMaximum.testMax(30.16f,56.12f,28.14f);
-        Assert.assertEquals(56.12f, result, 1.0);
+        FindMaximum findMaximum = new FindMaximum(28.14f,56.12f,30.16f);
+        Comparable result = findMaximum.testMax();
+        Assert.assertEquals(56.12f,result);
     }
     @Test
     public void givenFloatMaxNumber_AtThirdPosition_ShouldReturnSameNumber() {
-        FindMaximum findMaximum = new FindMaximum();
-        float result = findMaximum.testMax(30.16f,28.14f,56.12f);
-        Assert.assertEquals(56.12f, result, 1.0);
+        FindMaximum findMaximum = new FindMaximum(30.16f,28.14f,56.12f);
+        Comparable result = findMaximum.testMax();
+        Assert.assertEquals(56.12f,result);
     }
     @Test
     public void givenString_AtFirstPosition_ShouldReturnSameString() {
-        FindMaximum findMaximum = new FindMaximum();
-        String result = findMaximum.testMax("Orange","Mango","Banana");
+        FindMaximum findMaximum = new FindMaximum("Orange","Mango","Banana");
+        Comparable result = findMaximum.testMax();
         Assert.assertEquals("Orange",result);
     }
     @Test
     public void givenString_AtSecondPosition_ShouldReturnSameString() {
-        FindMaximum findMaximum = new FindMaximum();
-        String result = findMaximum.testMax("Mango","Orange","Banana");
+        FindMaximum findMaximum = new FindMaximum("Banana","Orange","Mango");
+        Comparable result = findMaximum.testMax();
         Assert.assertEquals("Orange",result);
     }
     @Test
     public void givenString_AtThirdPosition_ShouldReturnSameString() {
-        FindMaximum findMaximum = new FindMaximum();
-        String result = findMaximum.testMax("Banana","Mango","Orange");
+        FindMaximum findMaximum = new FindMaximum("Mango","Banana","Orange");
+        Comparable result = findMaximum.testMax();
         Assert.assertEquals("Orange",result);
     }
 
